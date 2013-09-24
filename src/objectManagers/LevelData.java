7182d4e013a6c3c092d4f51215b991daf8f57d01
Justin Oblak <jroblak@gmail.com>
2013-09-24 10:43:13 -0400

    add comments and walls

--- src/objectManagers/LevelData.java
+++ src/objectManagers/LevelData.java
@@ -54,24 +54,30 @@ public class LevelData {
 	public void loadLevel() {
 		
 		try {
+			// Get the Level JSON file and cast it to a JSONObject
 			Object raw = parser.parse(new FileReader("levels/level1.json"));
 			level = (JSONObject)raw; 
 			
-			levelTileHeight = (Long)level.get("tileheight");
-			levelTileWidth = (Long)level.get("tilewidth");
-			levelHeight = (Long)level.get("height");
-			levelWidth2 = (Long)level.get("width");
+			// Extract high-level level data from JSON
+			levelTileHeight = (Long)level.get("tileheight");  // Height of individual tiles
+			levelTileWidth = (Long)level.get("tilewidth");    // Width of individual tiles
+			levelHeight = (Long)level.get("height");          // Height of entire map
+			levelWidth2 = (Long)level.get("width");           // Width of entire map
 			
-			levelLayers = (JSONArray)level.get("layers");
-			levelTilesets = (JSONArray)level.get("tilesets");
+			levelLayers = (JSONArray)level.get("layers");     // Get JSON Array of "layers" of map
+			levelTilesets = (JSONArray)level.get("tilesets"); // Get JSON Array of the tileset information
 			
+			// Iterate through each of the tilesets and get relevant information from them
 			for(int i = 1; i <= levelTilesets.size(); i++) {
-				JSONObject tile = (JSONObject)levelTilesets.get(i - 1);
-				HashMap<String, Object> tileDef = new HashMap<String, Object>();
-				tileDef.put("gid", Integer.valueOf(tile.get("firstgid").toString()));
+				JSONObject tile = (JSONObject)levelTilesets.get(i - 1); // Get the i-1 tile
+				HashMap<String, Object> tileDef = new HashMap<String, Object>();  // Create a HashMap to put the data into
+				
+				// Add the relevant data from each tileset into the hashmap
+				tileDef.put("gid", Integer.valueOf(tile.get("firstgid").toString())); 
 				tileDef.put("image", tile.get("image").toString());
 				tileDef.put("props", tile.get("tileproperties"));
 				
+				// And add that tileset's information to list of tileset definitions
 				levelTileDefs.add(tileDef);
 			}
 			
@@ -81,19 +87,42 @@ public class LevelData {
 				long tileXPos = 0;
 				long tileYPos = 0;
 				
+				/* First, we check if the layer is visible, if it's not, we don't care because
+				 * we don't have to draw it.
+				 * TODO -- Add support for invisible collision layers
+				 */
 				if((Boolean) levelLayer.get("visible")) {
+					// Get the layer information
 					ArrayList<Long> tmpLevelData = (ArrayList<Long>) levelLayer.get("data");
 					
+					/* 		Iterate through the rows / columns of the level
+					 *   	NOTE: The level data array is a 1D array, but we want to process it like a 2D array
+					 *	 	j*levelWidth + k gets us the element in the 1D array as if it's a 2D array
+					 */
 					for(int j = 0; j < levelHeight; j++) {
 						for(int k = 0; k < levelWidth2; k++) {
-							if(tmpLevelData.get((int)((j*levelWidth2)+k)) == 0) continue;
+							if(tmpLevelData.get((int)((j*levelWidth2)+k)) == 0) continue; // If there is no data at the element, pass
 							else {
+								// Get the 2D position of the tile
 								tileXPos = levelTileWidth * k;
 								tileYPos = levelTileHeight * j;
+								
+								// Get the tiles value in the 1D array (see comment above about equation) ...
+								// Note: Idx refers to the fact that the value is used as an Index for the tiledefs array
+								//       NOTE that the value is the index of the tile in the leveldata array.
 								Long tileIdx = tmpLevelData.get((int)((j*levelWidth2)+k));
+								// .. and use it to access the the tile definition of that tile in the TileDefs JSON Object
+								// .. and get the relevant data
 								JSONObject tmp = (JSONObject)levelTileDefs.get(tileIdx.intValue()-1).get("props");
+								
+								/* TODO - Right now this is assuming that every image is a separate file and they all have 
+								 * 			and index of 0. If we use tilesets (which we should), I need to code in logic here
+								 * 			to select and create the correct "subimage" based on the values here.
+								 */
 								String imagePath = (String)levelTileDefs.get(tileIdx.intValue()-1).get("image");
 								
+								// Use an Iterator to loop through the levelDefs array and "dig" for property setting a tile to 
+								// either a floor or a wall
 								Iterator it = tmp.entrySet().iterator();
 								while (it.hasNext()){
 									Map.Entry pairs = (Map.Entry)it.next();
